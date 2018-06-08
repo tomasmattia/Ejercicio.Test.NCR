0de669cd1d0e840f8f5b8cc421f13ca367a5f830
Tomas Mattia <tomasmattia@users.noreply.github.com>
2018-06-08 00:46:34 -0300

    Update index.js

--- tyrion/playground/tests/index.js
+++ tyrion/playground/tests/index.js
@@ -2,8 +2,6 @@
 // tyrion/playground/tests/index.js
 const tyrion = require('tyrion');
 
-// require('./string-utils.test');
- //require('./number-utils.test');
 require('./test-qa.test');
 
 tyrion.end();
